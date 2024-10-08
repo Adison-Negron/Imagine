af7faf8844ece3735735056f468057ea44e85817
Axel Negron <axel.negron2@upr.edu>
2024-10-08 02:27:37 -0400

    Added alternative algorithm that generates specific wave types per rgb and generates sound based on color dominance and intensity. Results in variance in sounds
    Co-authored-by: Adrian Irizarry Negron <Adrian649@users.noreply.github.com>

--- Python/conversionmodules/imagetoaudio.py
+++ Python/conversionmodules/imagetoaudio.py
@@ -4,8 +4,10 @@ import os
 import glob
 import scipy.io
 import sys
+import random
+from scipy import signal
 
-def convolve(img_array, kernel, step=2):
+def convolve(img_array,out_path, kernel, step=2):
     x = img_array.shape[0]
     y = img_array.shape[1]
 
@@ -21,11 +23,120 @@ def convolve(img_array, kernel, step=2):
 
     return resultant_arr
 
-def generate_audio(arrays, out_path):
+# Function to ensure all arrays have the same dimension
+def ensure_same_dimension(arrays):
+    # Find maximum size
+    max_size = max(arr.shape[0] for arr in arrays)
+    
+    # Pad or truncate arrays to the same size
+    adjusted_arrays = []
+    for arr in arrays:
+        if arr.shape[0] < max_size:
+            adjusted_arrays.append(np.pad(arr, (0, max_size - arr.shape[0]), mode='constant'))
+        else:
+            adjusted_arrays.append(arr[:max_size])  # Truncate if larger
+    
+    return adjusted_arrays
+    
+
+def generate_rgb(img_array, out_path, kernel, step=2, intensity=0.9):
+    x = img_array.shape[0]
+    y = img_array.shape[1]
+
+    red_values = img_array[:, :, 0]
+    green_values = img_array[:, :, 1]
+    blue_values = img_array[:, :, 2]
+
+    rgb_dict = {'1': [], '2': [], '3': []}
+
+    # Calculate average values for each channel
+    for i in range(0, x - kernel + 1, step):
+        for j in range(0, y - kernel + 1, step):
+            avg_red = (np.mean(red_values[i:i + kernel, j:j + kernel]))+0.000000000001 / 256
+            avg_green = np.mean(green_values[i:i + kernel, j:j + kernel])+0.000000000001 / 256
+            avg_blue = np.mean(blue_values[i:i + kernel, j:j + kernel])+0.000000000001 / 256
+            
+            rgb_dict['1'].append(avg_red)
+            rgb_dict['2'].append(avg_green)
+            rgb_dict['3'].append(avg_blue)
+
+    # Compute overall average for each color channel
+    avg_red_overall = np.mean(rgb_dict['1'])
+    avg_green_overall = np.mean(rgb_dict['2'])
+    avg_blue_overall = np.mean(rgb_dict['3'])
+
+    # Find dominant color channel
+    dominant_color = max([('red', avg_red_overall), 
+                          ('green', avg_green_overall), 
+                          ('blue', avg_blue_overall)], key=lambda x: x[1])[0]
+    
+    weights = [.5, .2, .1]
+
+    templates = {
+        'pad': [weights[2], weights[1], weights[0]],
+        'lead': [weights[0], weights[1], weights[2]],
+        'string': [weights[1], weights[2], weights[0]]
+    }
+
+    audio_divisions = 10
+
+    # Split the RGB values into segments
+    subdivided_blue_array = np.array_split(rgb_dict["3"], audio_divisions)
+    subdivided_red_array = np.array_split(rgb_dict["1"], audio_divisions)
+    subdivided_green_array = np.array_split(rgb_dict["2"], audio_divisions)
+
+    pad_waves = []
+    lead_waves = []
+    string_waves = []
+
+    brightness = (avg_red_overall + avg_green_overall + avg_blue_overall)/3
+    
+    for i in range(audio_divisions):
+        pad_waves.append((np.sin(np.deg2rad(subdivided_blue_array[i]))))
+        lead_waves.append((signal.square(np.deg2rad(subdivided_red_array[i])))*.2)
+        string_waves.append((signal.sawtooth(np.deg2rad(subdivided_green_array[i]))))
+
+    concatenated_pad_waves = np.concatenate(pad_waves)
+    concatenated_lead_waves = np.concatenate(lead_waves)
+    concatenated_string_waves = np.concatenate(string_waves)
+
+    concatenated_pad_waves, concatenated_lead_waves, concatenated_string_waves = ensure_same_dimension([concatenated_pad_waves, concatenated_lead_waves, concatenated_string_waves])
+
+    
+    # Combine the waves based on the dominant color
+    match dominant_color:
+        case 'blue':
+            weights = templates['pad']
+            combined_wave = (np.multiply(concatenated_pad_waves, weights[2]) +
+                             (np.multiply(concatenated_lead_waves, weights[0]) / avg_blue_overall) +
+                             (np.multiply(concatenated_string_waves, weights[1]) / avg_blue_overall))
+        case 'green':
+            weights = templates["string"]
+            combined_wave = ((np.multiply(concatenated_pad_waves, weights[2]) / avg_green_overall) +
+                             (np.multiply(concatenated_lead_waves, weights[0]) / avg_green_overall) +
+                             (np.multiply(concatenated_string_waves, weights[1])))
+        case 'red':
+            weights = templates["lead"]
+            combined_wave = ((np.multiply(concatenated_pad_waves, weights[2]) / avg_red_overall) +
+                             (np.multiply(concatenated_lead_waves, weights[0])) +
+                             (np.multiply(concatenated_string_waves, weights[1]) / avg_red_overall))
+
+    sample_rate = 30000
+
+    #change the amplitude of the combined wave
+    combined_wave = combined_wave*intensity
+    # Apply the intensity factor to control the output waveform amplitude
+    random_id = random.randint(0,100)
+    # Normalize the combined wave to the range of int16 (to avoid distortion)
+    scipy.io.wavfile.write(f"{out_path}output{random_id}-{dominant_color}.wav", sample_rate, (combined_wave * 32767).astype(np.int16))  
+    
+    return combined_wave
+
+def generate_audio_nosplit(arrays, out_path):
     sample_rate = 44800
 
     for i in range(len(arrays)):
-        # Convert to radians
+
         array_radian = np.radians(arrays[i])
         
         # Generate sine and cosine wave
@@ -37,21 +148,20 @@ def generate_audio(arrays, out_path):
         combined_wave = array_sine + 0.5 * array_cosine
         
 
-
         # Normalize the combined wave to the range of int16 (to avoid distortion)
         max_value = np.max(np.abs(combined_wave))
         if max_value != 0:
             combined_wave_normalized = combined_wave / max_value
         else:
-            combined_wave_normalized = combined_wave  # In case max_value is 0, keep it unchanged
+            combined_wave_normalized = combined_wave  
 
         # Convert the normalized wave to int16
-        combined_wave_int16 = np.int16(combined_wave_normalized * 32767)  # scale to int16
-        sine_wave_int16 = np.int16(array_sine * 32767)  # scale to int16
-        cosine_wave_int16 = np.int16(array_cosine * 32767)  # scale to int16
-        square_wave_int16 = np.int16(array_square * 32767)  # scale to int16
+        combined_wave_int16 = np.int16(combined_wave_normalized * 32767) 
+        sine_wave_int16 = np.int16(array_sine * 32767) 
+        cosine_wave_int16 = np.int16(array_cosine * 32767)  
+        square_wave_int16 = np.int16(array_square * 32767) 
 
-        # Save as a .wav file in int16 format
+        # Saving files to disk
         scipy.io.wavfile.write(out_path + "output_combined_" + str(i) + ".wav", sample_rate, combined_wave_int16)
         scipy.io.wavfile.write(out_path + "output_sine_" + str(i) + ".wav", sample_rate, sine_wave_int16)
         scipy.io.wavfile.write(out_path + "output_cosine_" + str(i) + ".wav", sample_rate, cosine_wave_int16)
@@ -76,13 +186,15 @@ def imagetoaudio(img_path, out_path, kernel_size, step_size):
     convolution_results = []
     for i in range(len(Images)):
         print("Convolving image " + str(i) + " with Kernel Size " + str(kernel_size) + " and Step Size " + str(step_size))
-        results = convolve(Images[i], kernel_size, step_size)
-        convolution_results.append(results)
+        generate_rgb(Images[i], out_path, kernel_size, step_size)
+        # convolution_results.append(results)
 
-    generate_audio(convolution_results, out_path)
+
+
+    # generate_audio_nosplit(convolution_results, out_path)
 
     print("Finished")
-    return convolution_results
+    return 
 
 
 if __name__ == "__main__":
@@ -109,8 +221,8 @@ if __name__ == "__main__":
     else:
         img_path = os.getcwd() + "/imgs/"
         out_path = os.getcwd() + "/output/"
-        kernel_size = 100
-        step_size = 5
+        kernel_size = 10
+        step_size = 3
         imagetoaudio(img_path, out_path, kernel_size, step_size)
 
         print("Done")
